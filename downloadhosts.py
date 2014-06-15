7a4ef67e977c9ec7fc98ac1c8728854ae60a54c0
DapengWei <wdp137@gmail.com>
2014-06-15 22:33:04 +0800

    add anti-DNS pollution

--- downloadhosts.py
+++ downloadhosts.py
@@ -1,8 +1,36 @@
 # -*- coding: utf-8 -*-
 import urllib2
+import httplib
+import socket
+
+def MyResolver(host):
+  if host == 'simpleu.googlecode.com':
+    return '74.125.128.82' 
+  else:
+    return host
+
+class MyHTTPConnection(httplib.HTTPConnection):
+  def connect(self):
+    self.sock = socket.create_connection((MyResolver(self.host),self.port),self.timeout)
+
+class MyHTTPSConnection(httplib.HTTPSConnection):
+  def connect(self):
+    sock = socket.create_connection((MyResolver(self.host), self.port), self.timeout)
+    self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file)
+
+class MyHTTPHandler(urllib2.HTTPHandler):
+  def http_open(self,req):
+    return self.do_open(MyHTTPConnection,req)
+
+class MyHTTPSHandler(urllib2.HTTPSHandler):
+  def https_open(self,req):
+    return self.do_open(MyHTTPSConnection,req)
+
 proxy_handler = urllib2.ProxyHandler({'http': '127.0.0.1:1984'})
-opener = urllib2.build_opener(proxy_handler)
+
+opener = urllib2.build_opener(proxy_handler,MyHTTPHandler,MyHTTPSHandler)
 urllib2.install_opener(opener)
+
 print "downloading hosts with proxy"
 response = urllib2.urlopen("https://simpleu.googlecode.com/svn/trunk/hosts")
 data = response.read()
