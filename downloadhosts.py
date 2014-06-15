9eeb66b76a64d9fc131bdf88c33279efc4db8f18
Dapeng Wei <wdp137@gmail.com>
2014-06-15 22:47:07 +0800

    remove https connection and addon hosts file

--- downloadhosts.py
+++ downloadhosts.py
@@ -13,11 +13,6 @@ class MyHTTPConnection(httplib.HTTPConnection):
   def connect(self):
     self.sock = socket.create_connection((MyResolver(self.host),self.port),self.timeout)
 
-class MyHTTPSConnection(httplib.HTTPSConnection):
-  def connect(self):
-    sock = socket.create_connection((MyResolver(self.host), self.port), self.timeout)
-    self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file)
-
 class MyHTTPHandler(urllib2.HTTPHandler):
   def http_open(self,req):
     return self.do_open(MyHTTPConnection,req)
@@ -28,11 +23,11 @@ class MyHTTPSHandler(urllib2.HTTPSHandler):
 
 proxy_handler = urllib2.ProxyHandler({'http': '127.0.0.1:1984'})
 
-opener = urllib2.build_opener(proxy_handler,MyHTTPHandler,MyHTTPSHandler)
+opener = urllib2.build_opener(proxy_handler,MyHTTPHandler)
 urllib2.install_opener(opener)
 
 print "downloading hosts with proxy"
-response = urllib2.urlopen("https://simpleu.googlecode.com/svn/trunk/hosts")
+response = urllib2.urlopen("http://simpleu.googlecode.com/svn/trunk/hosts")
 data = response.read()
 with open("hosts", "wb") as hosts_file:     
     hosts_file.write(data) 
