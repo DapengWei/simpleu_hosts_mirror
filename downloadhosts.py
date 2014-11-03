e80cc8c48b25c6b4c9bc7216a2b7ed7fdf0f2ccf
Dapeng Wei <wdp137@gmail.com>
2014-11-03 11:18:01 +0800

    add new hosts from hosts.add

--- downloadhosts.py
+++ downloadhosts.py
@@ -32,3 +32,10 @@ data = response.read()
 with open("hosts", "wb") as hosts_file:     
     hosts_file.write(data) 
 print "downloading complete"
+
+print "begin to modify hosts"
+with open("hosts", "a") as hosts_file_org:
+	with open("hosts.add", "r") as hosts_file_add:
+		hosts_add_file = hosts_file_add.read()
+	hosts_file_org.write(hosts_add_file)
+print "modify hosts complete"
