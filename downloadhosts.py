d8cc49e20acf0a39acd2fb23cb301f8c1d5f3f4b
DapengWei <wdp137@gmail.com>
2014-11-11 21:13:55 +0800

    update 20140411 & use githubsource instead of googlecode

--- downloadhosts.py
+++ downloadhosts.py
@@ -1,34 +1,9 @@
 # -*- coding: utf-8 -*-
-import urllib2
-import httplib
-import socket
-
-def MyResolver(host):
-  if host == 'simpleu.googlecode.com':
-    return '74.125.128.82' 
-  else:
-    return host
-
-class MyHTTPConnection(httplib.HTTPConnection):
-  def connect(self):
-    self.sock = socket.create_connection((MyResolver(self.host),self.port),self.timeout)
-
-class MyHTTPHandler(urllib2.HTTPHandler):
-  def http_open(self,req):
-    return self.do_open(MyHTTPConnection,req)
-
-class MyHTTPSHandler(urllib2.HTTPSHandler):
-  def https_open(self,req):
-    return self.do_open(MyHTTPSConnection,req)
-
-proxy_handler = urllib2.ProxyHandler({'http': '10.241.10.132:1984'})
-
-opener = urllib2.build_opener(proxy_handler,MyHTTPHandler)
-urllib2.install_opener(opener)
+import requests
 
 print "downloading hosts with proxy"
-response = urllib2.urlopen("http://simpleu.googlecode.com/svn/trunk/hosts")
-data = response.read()
+r = requests.get("https://raw.githubusercontent.com/vokins/simpleu/master/hosts")
+data = r.content
 with open("hosts", "wb") as hosts_file:     
     hosts_file.write(data) 
 print "downloading complete"
@@ -38,4 +13,5 @@ with open("hosts", "a") as hosts_file_org:
 	with open("hosts.add", "r") as hosts_file_add:
 		hosts_add_file = hosts_file_add.read()
 	hosts_file_org.write(hosts_add_file)
+
 print "modify hosts complete"
